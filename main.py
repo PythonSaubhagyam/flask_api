ec2383d0997bf1df74d592175c4c71ea89eff600
PythonSaubhagyam <parth.saubhagyam@gmail.com>
2021-07-09 12:37:13 +0530

    multiple files as input

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 #     response = '\n'.join([message, version])
 #     return [response.encode()]
 
-
+import pymysql
 from flask import Flask,request,jsonify
 from werkzeug.utils import secure_filename
 import re
@@ -24,9 +24,49 @@ import os , io , sys
 import base64
 from io import BytesIO
 from PIL import Image
+from os.path import expanduser
+import paramiko
 
 import pytesseract
-#!/usr/bin/env python
+from sshtunnel import SSHTunnelForwarder
+sql_hostname = 'localhost'
+sql_username = 'uoka9vojujcjk'
+sql_password = 'admin1234!@#$'
+sql_main_database = 'dbhqpazymifxx6'
+sql_port = 3306
+ssh_host = 'monte-bianco.in'
+ssh_user = 'u30-3zfmzf4bsowc'
+ssh_port = 18765
+# sql_ip = '1.1.1.1.1'
+# mypkey = 'b3BlbnNzaC1rZXktdjEAAAAACmFlczI1Ni1jdHIAAAAGYmNyeXB0AAAAGAAAABCTGx4oOR6f5NRdWfKaEojaAAAAEAAAAAEAAAIXAAAAB3NzaC1yc2EAAAADAQABAAACAQDgq2t+7eVQzjKf4lrhmkn/T+FXR+XW3r8/ZvXwcjElW7ZDms1jz2KvJaIV3A51sJujnrRCZxlRJxI+Gk5oBWAPCYRikfJ3D9gHm50zh6ByzkgyrSTpn282j4KRU+FEsjSW+dmOza80G/kE4dFF9lOwzyOY/HB8RVyp+//CR+ByvyDe5PjKEkjodxX/N48CJlnr+ytYjI4Bwjj7/TVNtU0lix6LjshaqBFY/chYduyWzxUdbm1yiLwRN/59odBEXJQuHxGUghB5ljrRQdbh55yOHsk7msaz8b5ntI3swIVidO0A/R63q3K8jyEUZvfXlTFKB46096f9wdP/WBEov9rlkt+Q1LcLLLm6RIGsCpkmgcgRsOL7jClOxfMXXBHhqg/hXak74NDTT0goM3ufDLn/YDZy688WxPBxzH3QAMe5iK3K+sHst+P58zwB6KK40D171kdR5pOsk97PF/hUavC247MttvcmIdiWO6oUlqIJpTVRbzYLxzzBX1cHjUpYWCB0F+/LCtP9pbbJhJzTbfFW2CE0GjvpQ777RcXRtlK239dfbKNB6Q7lRxKzr7eiZMsFzLxHNLMxnR5edaXPfvmiIJWFMCta1hhAgRNmgv2Pez2YrdnyYrupWZq5Q7i8PFTnyyXI7kjAzXiuXeVVWo2uj8+0oioj9mUxdgn1QjceJwAAB0BILkpXfKbBEtbHRzw7L1jrcmXJ+FlNaC/cP0ZFLvWnRkQtRt2WwPfUt+mHMPLFobFLGDpMaXjBQJWjvEMfGO/AD9wc5u+cixqIPT0NAbaILXI/UELKM1i+zzTPGpvdKybsc9HVyZYQoV/jiDfrVHFPMaqCeDl8WksFzZXrdjOs7hpEeQlvUWJTxSlxXK0xlvWD4Rf/hFAcBHJQXvj1l6tI+Pl7S+ACoHvW+C0ne93RqTTQFjA2GBTf/DCiNEfFtCIfXwFNaQE+lObNfZQN7hNQRr9iOL8TkoSFIMBS3cHhRsLGxt9R4PXraDYLw7qOqyz1SiwBzg/nChzFP27cdlwCakE0qdZvvjHds6cwodVnIE3sXASs5psi37Cn2LcGtlA7UwsqD2Wq30w79/yLJLnO2G55XaM2p8IZQBRPjXhlMhl/8gSbhH636hfLA9TFb+nIyGXl5aAvPOhpvwlpIZ7NX34KbHGDBzilQM8pkbMxA/fA33SzkcTl2RqoW81XITMdR0fqEfyJSfBSbQV8hWl0VLibJiNwXsbyjXajPe7nJRrCxX+Qhd4TjFFehCPi8PpDjQxy6jC8kANGIagepDMZM3FP2mTwxQAnX10E6h4Vfyni+7D/y/ZWPshrz5AklwgZopTYDzp2THt78l5ns7oPnPY84E12qLpGLw0YJ6VFkfCNkopXdqj+IwrmVssBq3gHcTV+REZuGVV/9kqxqhztVPxotfJkUf3FxQEO6u8D86xy8KLwznc6TfNoQ3XxGu4vfOuGuVc0XD1W0aSUd2gNXaxLweUXuY6etO0yr+c7npjFHdZLMa9Ftg2Jvc7R+XaXRfRotq2hOy4VYrx7yT1Gc5V019vGdpalL7HVCOsYHhgGHYm+7sB7c+3t4sRETTWdWpk8qXH0qGzu+d0lVll8E64yeBZuhZ7p/eKu9oX3LznRJxM1PtCAVZTditT0TqlUm+ILKaJZXpU6OgfgyzTipYaZ8Av2FefmKeqFp4REztwjqXYzTA+QHXHH1zjeCG3fnDLR1OYgxRgVNtZtRZSEEsxHsBxZ1O1cqR0E34OfaNtFNB7ghF7owXK/N5V7QznBzKvnXy7zPP3/v3V7s4hpifZJVAS3I83zIXLKdKbk+3Y8chU+cMkrSB3cAyZodiS4CQF1AQroqEBQcOTL+XXe+WCT9tAjcKvee/6RKVBV/elG9Mr+7++vl0I/XVs/zGYgGVbEmGlIq9uNqPZgE+/Zl2xXURyvhOrGmR6GSRP+1UPgMP6eDMMGgkoet+z0wKXVgLieQ1FZpgNU9bKcft9sWGUAkLNMs5VmF+mra41Vovv7UV4bFWapcIDzKfBo6fGzu5LQ2sqWv4Wq+zLcFVdcfWji4Gf0N56iTYoiCr+DB9V/Vw4L7ajneNG95V1utGobMjahVfr59OJiVa9MW5as8tafs+Sxivuo/DeRZXPLTBPpcCZYA8Lk9gz/pX8H1OPNZ4vgabblg0SXcJK+E2Wz80255AOsyluB4ZG48QIGXlM8SLRVm43cdMh5DEfJAKl6bIi78+1objB2HrTVE6vpaFFGCB73LemcI2DSw/f6UGgyhe8+CCS3QIjxh8BkJ6tupHW3LssAwuypzV7at5cKUT8TgnKErIuLyHA0DJ4vfyoP4cDVWc7gtzLwimD4FwJhxM6V77rAb45vDdKjU0asVQLQwL6Kka87rVC32n29wKPsZMedsXkh5dCX1c7ATHAk5fXbk6dsRHKvt1Egj/FqEFrXaYKG3wwZScmCj1D52iZxn/nVNBh4kmATwCymZ93Ma3+s3Gm3QR5mmALdLu/O/ZFF1rqfD0OU4SVd8Ba/N9BqenjedyaprBOtlRd5dP5ahxO6vbFVUXWUK6mr4gbbEnUMnT6d6GdbEQ00li+pI034yKtSgB+wVtAdFCqW0M3xJiQB+l6zrsp2O5B+QBpnC+cF/nqjetpwARvDCzOzyOuSD2EhHuLTmVfhKbAQ1dmJSWJkoYjEz9PE1KLIiyXvWpH9yuunWAUbEROABKx4uQPHF6kie0+FAQ6ibmTlxTqitsQ0xUlTAbqLoIACl8/2yVxHDqAsJy6TE6hii4aZcb9qgOqmVoSBFUg4b43wksiu40sDgAvhBszymKlcYX0RdGYk4baivDhR7vWUwRvQnlaRtBD5UxvbPwUGFuG6jGcuUhUWIOhBLlEs8sAaD1tAdh3qUhZx3z7N23zr9rpuhjBZQCpmqBIjJ3m9mZpdA2S78MIIWNM48gbXREkorOuG93szfKzbNNf2sc/en7rsomoLkECYi6ON54VwBLiT9XCXKLI6rUx/6DsCnLiDzF5FzSUQjmeo+563DvDBzADeicJhmR3l7ySL5tX9pld+a2UiWLVXCnGI2AIxUAe4vKkdszAZp8Z0y7ggB0Q5o0ZAv0XxSG1Pv69kPw1/aAEeLs+Am1VVAXXXQfGxttHKGenFizg5A9lbP9/QMBsQYo9SNg=='
+home = expanduser('~')
+privatekeyfile = os.path.expanduser('~/.ssh/id_rsa')
+mypkey = paramiko.RSAKey.from_private_key_file(home + '/Documents/docsender_key.ppk',password=';p@f2&42*^@$')
+
+
+
+with SSHTunnelForwarder(
+        (ssh_host, ssh_port),
+        ssh_username=ssh_user,
+        ssh_pkey=mypkey,
+        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+    conn = mysql.connector.connect(host='localhost', user=sql_username,
+            passwd=sql_password, db=sql_main_database,
+            port=tunnel.local_bind_port)
+    cursor = conn.cursor()
+
+    cursor.execute('set max_allowed_packet=67108864')
+
+# with SSHTunnelForwarder(('dbhqpazymifxx6', 18765),
+#                   ssh_password=";p@f2&42*^@$",
+#                   ssh_username="u30-3zfmzf4bsowc",
+#                   remote_bind_address=('localhost',  18765)) as server:
+#                     conn = mysql.connector.connect(
+#                             host='monte-bianco.in',
+#                             port=' 18765',
+#                             user='uoka9vojujcjk',
+#                             password='admin1234!@#$',
+#                             database='dbhqpazymifxx6')
 
 
 
@@ -275,11 +315,11 @@ def allowed_file(filename):
         return '.' in filename and \
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# UPLOAD_FOLDER = '/home/thebugsb/restapi/uploads'
+UPLOAD_FOLDER = '/home/python/Downloads/restful_api/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg','pdf','txt'])
 
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER']='/home/thebugsb/restapi/uploads'
+app.config['UPLOAD_FOLDER']='/home/python/Downloads/restful_api/uploads'
   
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
@@ -294,7 +334,16 @@ def upload():
     document_type=request.form.get('document_type')
     if str(document_type)=='Sales':
         user_id = request.form.get('user_id')
-        file= request.form.get('file')
+        file1 = request.form.get('file')
+        file2 = request.form.get('file')
+        file3 = request.form.get('file')
+        file4 = request.form.get('file')
+        if file2=='':
+            pass
+        if file3=='':
+            pass
+        if file4=='':
+            pass
         select_date=request.form.get('date')
         Invoice_No=request.form.get('invoice_no')
         Invoice_Value=request.form.get('invoice_value')
@@ -307,7 +356,16 @@ def upload():
             pass
     elif str(document_type)=='Purchase':
         user_id = request.form.get('user_id')
-        file= request.form.get('file')
+        file1 = request.form.get('file')
+        file2 = request.form.get('file')
+        file3 = request.form.get('file')
+        file4 = request.form.get('file')
+        if file2 == '':
+            pass
+        if file3 == '':
+            pass
+        if file4 == '':
+            pass
         select_date=request.form.get('date')
         Invoice_No=request.form.get('invoice_no')
         Invoice_Value=request.form.get('invoice_value')
@@ -320,7 +378,16 @@ def upload():
             pass
     elif str(document_type)=='Credit':
         user_id = request.form.get('user_id')
-        file= request.form.get('file')
+        file1 = request.form.get('file')
+        file2 = request.form.get('file')
+        file3 = request.form.get('file')
+        file4 = request.form.get('file')
+        if file2 == '':
+            pass
+        if file3 == '':
+            pass
+        if file4 == '':
+            pass
         select_date=request.form.get('date')
         Credit_No=request.form.get('credit_no')
         Credit_Value=request.form.get('credit_value')
@@ -333,7 +400,16 @@ def upload():
             pass
     elif str(document_type)=='Debit':
         user_id = request.form.get('user_id')
-        file= request.form.get('file')
+        file1 = request.form.get('file')
+        file2 = request.form.get('file')
+        file3 = request.form.get('file')
+        file4 = request.form.get('file')
+        if file2 == '':
+            pass
+        if file3 == '':
+            pass
+        if file4 == '':
+            pass
         select_date=request.form.get('date')
         Debit_No=request.form.get('debit_no')
         Debit_Value=request.form.get('debit_value')
@@ -346,7 +422,16 @@ def upload():
             pass
     else:
         user_id = request.form.get('user_id')
-        file= request.form.get('file')
+        file1 = request.form.get('file')
+        file2 = request.form.get('file')
+        file3 = request.form.get('file')
+        file4 = request.form.get('file')
+        if file2 == '':
+            pass
+        if file3 == '':
+            pass
+        if file4 == '':
+            pass
         select_date=request.form.get('date')
         # select_date=request.form.get('Select Date')
         # Invoice_No=request.form.get('Invoice No')
